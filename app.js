2b0bede65abb81debce4a7e12c4c6bc410cf0fee
Ivan-Pavlukhin <pavlukhinivan@gmail.com>
2021-08-29 19:23:02 +0300

    Added start files to conect DB.

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const { DB_HOST } = require("./config")
+
+const app = express()
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('<h2>Home page</h2>')
+})
+
+
+
+mongoose.connect(DB_HOST, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        app.listen(3000)
+    })
+    .catch(error => console.log(error))
