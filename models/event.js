36e0ef819f1a65bf61038623523537ca80804158
Ivan-Pavlukhin <pavlukhinivan@gmail.com>
2021-09-08 17:04:56 +0300

    fix model

--- models/event.js
+++ models/event.js
@@ -1,41 +1,42 @@
-const { Schema, model } = require('mongoose')
-const Joi = require('joi')
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
 
-const eventSchema = Schema({
-  eventName: {
-    type: String,
-    required: [true, 'Название зала нужно указать обязательно']
+const eventSchema = Schema(
+  {
+    eventName: {
+      type: String,
+      required: [true, "Название концерта нужно указать обязательно"],
+    },
+    city: {
+      type: String,
+      required: [true, "Название город нужно указать обязательно"],
+    },
+    dateEvent: {
+      type: String,
+      require: [true, "Дату проведения нужно указывать обязательно"],
+      // unique: true,
+      // match: /^\([0-9]{3}\)\s[0-9]{3}-[0-9]{4}$/
+      // minlength: 15,
+      // maxlength: 15
+      // "2021-12-18T13:00"
+    },
+    hallName: {
+      type: String,
+      required: [true, "Название зала нужно указівать обязательно"],
+    },
+    aboutEvent: {
+      type: String,
+    },
+    size: {
+      type: Object,
+    },
+    active: {
+      type: Boolean,
+      require: true,
+    },
   },
-  city: {
-    type: String,
-    required: [true, 'Название город нужно указать обязательно']
-  },
-  dateEvent: {
-    type: String,
-    require: [true, 'Дату проведения нужно указывать обязательно']
-    // unique: true,
-    // match: /^\([0-9]{3}\)\s[0-9]{3}-[0-9]{4}$/
-    // minlength: 15,
-    // maxlength: 15
-    // "2021-12-18T13:00"
-  },
-  hallName: {
-    type: String,
-    required: [true, 'Название зала нужно указівать обязательно'],
-  },
-  aboutEvent: {
-    type: String,
-  },
-  size: {
-    type: {
-      // массив с объектами рядов => масив с объектами мест => место это id, price, active, reserve.
-    }
-  },
-  active: {
-    type: Boolean,
-    require: true
-  }
-}, { versionKey: false, timestamps: true })
+  { versionKey: false, timestamps: true }
+);
 
 const joiEventSchema = Joi.object({
   eventName: Joi.string().required(),
@@ -44,9 +45,9 @@ const joiEventSchema = Joi.object({
   aboutEvent: Joi.string().required(),
   dateEvent: Joi.date(),
   size: Joi.object(),
-  active: Joi.boolean()
-})
+  active: Joi.boolean(),
+});
 
-const Event = model('event', eventSchema)
+const Event = model("event", eventSchema);
 
-module.exports = { Event, joiEventSchema }
+module.exports = { Event, joiEventSchema };
